b5cc1f0e4d5a197c30355d6cae6bd13901a0bfa1
chaudhuree <chaudhuree00@gmail.com>
2024-04-04 02:02:16 +0600

    updated backend server's socket io's origin

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,7 @@ console.log(`Server is listening on port ${port}...`)
 const io = socketio(server,{
   pingTimeout: 60000,
   cors: {
-      origin: ['http://localhost:5173'],
+      origin: ['http://localhost:5173','https://yourtodo-chaudhuree.onrender.com'],
       // credentials: true,
   },
 });
